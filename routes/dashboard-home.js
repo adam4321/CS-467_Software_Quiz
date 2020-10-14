1d3bbc8ca5704dda85ca8badb04ba25d87067563
Adam Wright <adam@adamjwright.com>
2020-10-14 16:03:09 -0700

    Add Facebook login

--- routes/dashboard-home.js
+++ routes/dashboard-home.js
@@ -1,5 +1,5 @@
 /******************************************************************************
-**  Description: DASHBOARD HOME - server side node.js routes
+**  Description: DASHBOARD HOME / JOB POSTING - server side node.js routes
 **
 **  Root path:  localhost:3500/dashboard
 **
@@ -20,8 +20,20 @@ const checkUserLoggedIn = (req, res, next) => {
 // INITIAL DASHBOARD - Function to render the main dashboard --------------- */
 function renderDashboard(req, res, next) {
     let context = {};
-    context = req.user;
-    console.log(context);
+
+    // Test for the auth provider (Google vs Facebook) and create context object
+    if (req.user.provider === 'google') {
+        context.email = req.user.email;
+        context.name = req.user.displayName;
+        context.photo = req.user.picture;
+    } else {
+        context.email = req.user.emails[0].value;
+        context.name = req.user.displayName;
+        context.photo = req.user.photos[0].value;
+    }
+
+    console.log(req.user);
+    // console.log(context);
 
     res.render("dashboard-home", context);
 };
