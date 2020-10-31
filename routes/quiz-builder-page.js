4c8eea70d6f150608c752c3ff71bab3927106a0b
Kevin <hill.kevin@earthlink.net>
2020-10-30 22:29:09 -0400

    KJH rename commnet in debug constant to debug flag

--- routes/quiz-builder-page.js
+++ routes/quiz-builder-page.js
@@ -13,11 +13,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 // Debug Flag
-=======
-// Debug constant
->>>>>>> sample-quiz
 DEBUG = 0;
 
 // Get schemas
