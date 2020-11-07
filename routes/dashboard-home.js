3b6950cfa888672c51bceb95ed7ecd2b49beb6e8
Kevin <hill.kevin@earthlink.net>
2020-11-06 23:06:34 -0500

    KJH added save candidate to database and finsihed form values to msg object

--- routes/dashboard-home.js
+++ routes/dashboard-home.js
@@ -31,6 +31,7 @@ var DEBUG = 1;
 // Get schemas
 const JobPosting = require('../models/jobposting.js');
 const Employer = require('../models/employer.js');
+const Candidate = require('../models/candidate.js');
 const Quiz = require('../models/quiz.js');
 
 
@@ -39,9 +40,10 @@ const checkUserLoggedIn = (req, res, next) => {
     req.user ? next(): res.status(401).render('unauthorized-page', {layout: 'login'});
 }
 
+// INITIAL DASHBOARD - Function to query schema that is used more than once on the main dashboard --------------- */
 function renderPageFromQuery(req, res, next, context, user_id, emp_new){
-    // Find object one object in job postings data model
-    JobPosting.find({})//.lean().where('associatedQuiz.employer_id').equals(user_id)
+    // Find  all objects in job postings data model
+    JobPosting.find({})
     .exec()
     .then(doc => {
         context.jobposting = doc;
@@ -124,6 +126,86 @@ function renderDashboard(req, res, next) {
     });
 };
 
+// INITIAL DASHBOARD - Function send quiz link email to the candidate using SendGrid on the main dashboard --------------- */
+function sendQuizLinkEmail(req, res, next, msg) {
+    console.log(msg);
+    if (DEBUG === 0){
+        sgMail.send(msg)
+        .then(() => {
+            console.log('Email sent')
+            res.status(200).redirect('/dashboard');
+        })
+        .catch((error) => {
+            console.error(error)
+            res.status(500).redirect('/dashboard');
+        });
+        }
+        else{
+            res.status(200).redirect('/dashboard');
+        }
+}
+
+// INITIAL DASHBOARD - Function to process quiz parameters and store candidate details in database on the main dashboard --------------- */
+function readEmailForm(req, res, next) {
+    // TODO: query jobposting and place quiz in jobposting if not already there in AssociatedQuiz
+    let first = req.body.first;
+    let last = req.body.last;
+    let email = req.body.email;
+    let job_arr = req.body.jobposting.split(", ");
+    let jobposting = job_arr[0];
+    let title = job_arr[1];
+    let quiz = req.body.quiz;
+    var payload = { email: email, jobposting: jobposting, quiz: quiz};
+    var token = jwt.encode(payload, CRED_ENV.HASH_SECRET);
+    let quiz_link = 'https://softwarecustomquiz.herokuapp.com/take_quiz/'+token;
+    let message = first + ' ' + last + ',' +' Please click the following to take the employer quiz '+ quiz_link +' ';
+    let html_message = '<strong>' +message + '</strong>';
+    let name = 'Invitation to Take ' + title + ' Aptitude Quiz';
+    const msg = {
+        to: `${email}`, // Recipient
+        from: 'software.customquiz@gmail.com', // Verified sender
+        subject: `${name}`,
+        text: `${message}`,
+        html: `${html_message}`,
+    }
+
+    // Save new object to database collection
+    const cand = new Candidate({
+        _id: new mongoose.Types.ObjectId,
+        email: email,
+        firstName: first,
+        lastName: last,
+        quizResponseId: []
+    });
+
+    // Check if email is already registered in collection/employers
+    var query = Candidate.find({});
+    query.where('email').equals(email);
+    query.exec()
+    .then(result => {
+        // No email found
+        if (result[0] == undefined) {
+            cand.save()
+            .then(result => {
+                sendQuizLinkEmail(req, res, next, msg);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).render("dashboard-home", context);
+            });
+        }
+        else{
+            // Email already exists
+            sendQuizLinkEmail(req, res, next, msg);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).render("dashboard-home", context);
+    });
+
+};
+
 
 /*
 SAMPLES 
@@ -181,43 +263,7 @@ quizResponses : [{
 
 router.get('/', checkUserLoggedIn, renderDashboard);
 
-// INITIAL DASHBOARD - Function to send quiz link to candidate --------------- */
-router.post('/sendmail', checkUserLoggedIn, (req, res)=>{
-    console.log(req.body);
-    let email = req.body.email;
-    let jobposting = req.body.jobposting;
-    let title = req.boby.title;
-    let quiz = req.body.quiz;
-    var payload = { email: email, jobposting: jobposting, quiz: quiz};
-    var token = jwt.encode(payload, CRED_ENV.HASH_SECRET);
-    console.log();
-    console.log(token);
-    let message = 'Testing';
-    let html_message = message;
-    let name = 'Invitation to Take ' + title + ' Aptitude Quiz';
-    const msg = {
-        to: `${email}`, // Recipient
-        from: 'software.customquiz@gmail.com', // Verified sender
-        subject: `${name}`,
-        text: `${message}`,
-        html: `${html_message}`,
-    }
+router.post('/sendmail', checkUserLoggedIn, readEmailForm);
 
-
-    if (DEBUG === 0){
-    sgMail.send(msg)
-    .then(() => {
-        console.log('Email sent')
-        res.status(200).redirect('/dashboard');
-    })
-    .catch((error) => {
-        console.error(error)
-        res.status(500).redirect('/dashboard');
-    });
-    }
-    else{
-        res.status(200).redirect('/dashboard');
-    }
-});
 
 module.exports = router;
